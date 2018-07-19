e66f2bb85b41e922b3bf88c23155061aa64a9c80
Charbel <charbelbathiche@gmail.com>
2018-07-19 13:38:56 +0300

    Fixed lang field

--- resources/assets/js/components/domupdate.vue
+++ resources/assets/js/components/domupdate.vue
@@ -15,8 +15,8 @@
             </div> 
 
 
-            <div  style="width: 111% !important;margin-left: 9%;">
-                <label for="language"> Language:</label> 
+            <div  style="width: 111% !important;display: -webkit-box;padding: 0px;">
+                <label for="language" style="margin-right: 15px;"> Language:</label> 
 
                 <v-select
                   :options="['Chinese','Spanish','English','Hindi','Arabic','Portuguese','Turkish','Russian','Japanese','German','Italian']"
@@ -91,3 +91,47 @@
         
     }
 </script>
+
+
+<style>
+
+/*.v-select .dropdown-toggle {
+    -webkit-appearance: none;
+    -moz-appearance: none;
+    appearance: none;
+    width: 20%;
+    display: block;
+    padding: 0;
+    background: none;
+    border: 1px solid rgba(241, 39, 39, 0.26);
+    border-radius: 4px;
+    white-space: normal;
+}
+.form-control {
+    display: block;
+    width: 90px !important;
+    font-size: 1rem;
+    line-height: 1.5;
+    color: rgb(73, 80, 87);
+    background-color: rgb(255, 255, 255);
+    background-clip: padding-box;
+    padding: 0.375rem 0.75rem;
+    border-width: 1px;
+    border-style: solid;
+    border-color: rgb(206, 212, 218);
+    border-image: initial;
+    border-radius: 0.25rem;
+    transition: border-color 0.15s ease-in-out, box-shadow 0.15s ease-in-out;
+}*/
+.dropdown v-select single searchable
+{
+    display: block;
+    width:250px !important;
+}
+.dropdown-toggle clearfix
+{
+     width: 500px;
+
+}
+
+    </style>
